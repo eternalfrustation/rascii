1d0cdafb8f2ca1ab50a0f0d31a07154c9c20a024
Sandeep Kumar <sandeepqmar05@gmail.com>
2025-03-18 11:31:26 +0530

    Moved to a pest grammar

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use pest::{Parser, iterators::Pair};
+use std::fmt::Debug;
+use std::fs;
+use std::hash::Hash;
+
+use lexer::Rule;
+
+pub mod lexer;
+fn main() {
+    let contents = fs::read_to_string("test.adoc").expect("Something went wrong reading the file");
+    let lexer = lexer::AsciiDocParser::parse(Rule::document, &contents)
+        .expect("unsuccessful parse")
+        .next()
+        .unwrap();
+    print_pair(lexer, 5);
+}
+
+fn print_pair<R>(pair: Pair<R>, level: u8)
+where
+    R: Copy,
+    R: Debug,
+    R: Eq,
+    R: Hash,
+    R: Ord,
+{
+    if level < 1 {
+        return;
+    }
+    println!("Rule:    {:?}", pair.as_rule());
+    println!("Span:    {:?}", pair.as_span());
+    println!("Text:    {}", pair.as_str());
+    for inner_pair in pair.into_inner() {
+        print_pair(inner_pair, level - 1);
+    }
+}
